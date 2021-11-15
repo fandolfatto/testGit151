3c2fd9db8e7a202c9e060acafba729ba78d055e7
Frédérique Andolfatto <frederique.andolfatto@cpnv.ch>
2021-11-15 16:46:00 +0100

    test

--- view/login.php
+++ view/login.php
@@ -6,3 +6,31 @@
  * @author    Created by Frederique.ANDOLFATT
  * @version   15.11.2021
  */
+?>
+<!-- content page -->
+	<section class="bgwhite p-t-66 p-b-60">
+		<div class="container">
+			<div class="row">
+
+				<div class="col-md-12 p-b-30">
+					<form class="leave-comment">
+						<h4 class="m-text26 p-b-36 p-t-15">
+                            Connectez-vous
+						</h4>
+
+						<div class="bo4 of-hidden size15 m-b-20">
+							<input class="sizefull s-text7 p-l-22 p-r-22" type="email" name="email" placeholder="Adresse email">
+						</div>
+
+						<div class="bo4 of-hidden size15 m-b-20">
+							<input class="sizefull s-text7 p-l-22 p-r-22" type="password" name="userPswd" placeholder="Mot de passe">
+						</div>
+						<input type="submit" value="login" class="flex-c-m size2 bg4 bo-rad-23 hov1 m-text3 trans-0-4"><br>
+						<input type="reset" value="Annuler" class="flex-c-m size2 bg4 bo-rad-23 hov1 m-text3 trans-0-4">
+
+					</form>
+				</div>
+			</div>
+		</div>
+	</section>
+
